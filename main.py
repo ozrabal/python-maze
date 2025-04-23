1d25e713d31ba07a050c36ca07e03feff72d4dd8
Piotr Łepkowski <ozrabal@gmail.com>
2025-04-23 22:48:46 +0200

    Add initial implementation of maze application using Tkinter

---
+++ main.py
@@ -0,0 +1,30 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width=800, height=600):
+        self.__root = Tk()
+        self.__root.title("Maze")
+        self.canvas = Canvas(self.__root, width=width, height=height)
+        self.canvas.pack(fill=BOTH, expand=1)
+        self.running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+        
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+            
+    def close(self):
+        self.running = False
+        self.__root.destroy()
+
+def main():
+    win = Window(800, 600)
+    win.wait_for_close()
+
+if __name__ == "__main__":
+    main()
